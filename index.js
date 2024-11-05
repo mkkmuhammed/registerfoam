218d6068dabd9e20ee358d3579a11112b87c3ced
mkkmuhammed <mkkmuhammed8@gmail.com>
2024-11-05 10:24:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+      function jump(event) {
+           event.preventDefault(); // Prevent form from submitting the traditional way
+           window.location.href = "index2.html"; // Replace with the target page's URL
+        }
+
+ 
